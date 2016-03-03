9a7024302437de4c8025f26912d0c14fb93fea38
IIIJIaHr <midav.lis@gmail.com>
2016-03-03 17:53:54 +0300

    forgotten file

--- app/js/main.js
+++ app/js/main.js
@@ -26,8 +26,7 @@ require.config({
         underscore: '../components/underscore/underscore',
         backbone: '../components/backbone/backbone',
         hbs: '../components/require-handlebars-plugin/hbs',
-        handlebars: '../components/require-handlebars-plugin/hbs/handlebars',
-        text: '../components/text/text'
+        handlebars: '../components/handlebars/handlebars',
     }
 });
 
