061e797ace6c9b904b651dfe10a1154fda661d2a
IIIJIaHr <midav.lis@gmail.com>
2016-03-07 23:12:36 +0300

    Revert "some corrections grunt"
    This reverts commit 734f204f4eeb92df4bd06d0999652dea3550c34e.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,87 +1,76 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    wiredep: {
-      target: {
-        src: 'index.html'
-      },
-      options: {
-        exclude: [
-          'app/components/requirejs/require.js',
-          // 'app/components/jquery/dist/jquery.js',
-          'app/components/underscore/underscore.js',
-          'app/components/backbone/backbone.js',
-          'app/components/handlebars/handlebars.js',
-          'app/components/bootstrap-css/js/bootstrap.min.js'
-        ]
-      }
-    },
+    // Project configuration.
+    grunt.initConfig({
+            pkg: grunt.file.readJSON('package.json'),
+            // wiredep: {
+            //     target: {
+            //         src: 'index.html'
+            //     },
+            //     options: {
+            //         exclude: ['app/components/requirejs/require.js']
+            //     }
+            // },
 
-    // jshint: {
-    //     options: {
-    //         "asi": false,
-    //         "bitwise": true,
-    //         "boss": false,
-    //         "curly": true,
-    //         "debug": false,
-    //         "devel": false,
-    //         "eqeqeq": true,
-    //         "evil": false,
-    //         "expr": true,
-    //         "forin": false,
-    //         "immed": true,
-    //         "latedef": false,
-    //         "laxbreak": false,
-    //         "multistr": true,
-    //         "newcap": true,
-    //         "noarg": true,
-    //         "node": true,
-    //         "browser": true,
-    //         "noempty": false,
-    //         "nonew": true,
-    //         "onevar": false,
-    //         "plusplus": false,
-    //         "regexp": false,
-    //         "strict": false,
-    //         "sub": false,
-    //         "trailing": true,
-    //         "undef": true,
-    //         globals: {
-    //             jQuery: true,
-    //             Backbone: true,
-    //             _: true,
-    //             $: true,
-    //             require: true,
-    //             define: true
-    //         }
-    //     },
-    //     js: ['app/js/**/*.js']
-    // },
+            // jshint: {
+            //     options: {
+            //         "asi": false,
+            //         "bitwise": true,
+            //         "boss": false,
+            //         "curly": true,
+            //         "debug": false,
+            //         "devel": false,
+            //         "eqeqeq": true,
+            //         "evil": false,
+            //         "expr": true,
+            //         "forin": false,
+            //         "immed": true,
+            //         "latedef": false,
+            //         "laxbreak": false,
+            //         "multistr": true,
+            //         "newcap": true,
+            //         "noarg": true,
+            //         "node": true,
+            //         "browser": true,
+            //         "noempty": false,
+            //         "nonew": true,
+            //         "onevar": false,
+            //         "plusplus": false,
+            //         "regexp": false,
+            //         "strict": false,
+            //         "sub": false,
+            //         "trailing": true,
+            //         "undef": true,
+            //         globals: {
+            //             jQuery: true,
+            //             Backbone: true,
+            //             _: true,
+            //             $: true,
+            //             require: true,
+            //             define: true
+            //         }
+            //     },
+            //     js: ['app/js/**/*.js']
+            // },
 
-    watch: {
-      script: {
-        files: [
-          'app/**/*/*.js',
-          'app/**/*/*.css',
-          'index.html',
-          'Gruntfile.js'
-        ],
-        options: {
-          tasks: ['wiredep'],
-          livereload: true,
-        },
-      }
-    }
-  });
+            watch:{
+                script: {
+                    files: ['app/**/*/*.js', 'app/**/*/*.css'],
+                        options: {
+                        livereload: true,
+                    },
+                }
+            }
+        }
+    );
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-wiredep');
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // Load the plugin that provides the "uglify" task.
+    // grunt.loadNpmTasks('grunt-wiredep');
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
-  grunt.registerTask('default', ['watch', 'wiredep']);
+
+    // Default task(s).
+    // grunt.registerTask('default', ['wiredep', 'jshint']);
 
 };
